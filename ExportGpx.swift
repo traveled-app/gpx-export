ad05278beb3b91fdb0775257fe9f3408ffa78d41
david <you@example.com>
2021-02-10 12:02:54 -0800

    Optimized loading of GPX file header

--- ExportGpx.swift
+++ ExportGpx.swift
@@ -34,43 +34,36 @@ class MessageWithSubject: NSObject, UIActivityItemSource {
 }
 
 @objcMembers class ExportGPX:NSObject {
+    static let header = ["<?xml version=\"1.0\" encoding=\"UTF-8\" ?>",
+                         "<gpx xmlns:gpxx=\"http://www.garmin.com/xmlschemas/GpxExtensions/v3\"",
+                         "creator=\"Traveled App\"",
+                         "version=\"1.1\"",
+                         "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"",
+                         "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\">",
+                         ""
+    ].reduce("", {(result, line) in return result + line + "\n"})
+    
     static func export(locations:[CLLocation], presenter:UIViewController) {
-        NSLog("Export %ld events", locations.count)
         if locations.count > 0 {
-            let header = ["<?xml version=\"1.0\" encoding=\"UTF-8\" ?>",
-                          "<gpx xmlns:gpxx=\"http://www.garmin.com/xmlschemas/GpxExtensions/v3\"",
-                          "creator=\"Traveled App\"",
-                          "version=\"1.1\"",
-                          "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"",
-                          "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd\">",
-                          ""
-            ]
             let file = "traveled.gpx"
             if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
                 let fileURL = dir.appendingPathComponent(file)
-                //writing
-                var gpx = ""
-                
-                // gpx xml header
-                for line in header {
-                    gpx += line
-                    gpx += "\n"
-                }
-                // gpx locations
+                var gpx = header
+                // append gpx locations
                 for i in locations {
                     // try "wpt" attributes: name, cmt, desc
                     gpx += "  <wpt lat=\"\(i.coordinate.latitude)\" lon=\"\(i.coordinate.longitude)\"></wpt>\n"
                 }
                 gpx += "\n</gpx>\n"
-                do{
+                do {
                     // write file
                     try gpx.write(to: fileURL, atomically: false, encoding: .utf8)
-                }catch{
+                }
+                catch {
                     print("error writing")
                 }
                 // set up activity view
                 let mws = MessageWithSubject.init(subject: "Traveled GPX File Export", message: "Attached is a GPX file containing the location of every place visited while using the Traveled app.")
-                
                 // present activity view
                 let activ = UIActivityViewController.init(activityItems: [mws, fileURL], applicationActivities: nil)
                 //activ.excludedActivityTypes = [UIActivityViewController.Type]
@@ -83,8 +76,7 @@ class MessageWithSubject: NSObject, UIActivityItemSource {
         }
     }
     
-    static func alertError(message: String, presenter: UIViewController)
-    {
+    static func alertError(message: String, presenter: UIViewController) {
         let ac = UIAlertController.init(title: "Notice", message: message, preferredStyle: UIAlertController.Style.alert)
         ac.addAction(UIAlertAction.init(title: "OK", style: UIAlertAction.Style.destructive, handler: nil))
         presenter.present(ac, animated: true, completion: nil)
